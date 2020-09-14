161a4ac03f725e52f79446f14bfc82db0850b848
kasra-pv <kasra_sh94@protonmail.com>
2020-09-14 13:53:44 +0430

    fixed/updated dependency

--- template/fmt.js
+++ template/fmt.js
@@ -1,6 +1,6 @@
 const T = require("../core/types")
-const I = require("../core/iter");
-const Lex = require("kenpo-js/Kenpo").Lexer,
+const I = require("../core/streams");
+const Lex = require("kenpo-js/src/lexer/Tokenizer"),
     Tokenizer = Lex.Tokenizer,
     TokenType = Lex.TokenType;
 
