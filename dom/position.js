8aeaad2d63c5d350813cd5f80d845f1e96277186
kasra-sh <kasra.sh13@gmail.com>
2020-09-23 23:51:00 +0330

    in progress

--- dom/position.js
+++ dom/position.js
@@ -1,22 +1,34 @@
-let E = {};
+const {cls} = require("../dom/classes")
+const {attrs} = require("../dom/attributes")
 
 function left(e) {
     return e.offset
 }
+
 function leftW(e) {
     return e.clientLeft
 }
+
 function width(e) {
     return e.offsetWidth
 }
+
 function widthW(e) {
     return e.clientWidth
 }
+
 function right(e) {
     return left(e) + width(e)
 }
+
 function rightW(e) {
     return leftW(e) + widthW(e)
+}
+
+function translate(e, {x,y,z}) {
+    if (x) {
+
+    }
 }
 
 module.exports = {left, width, right}
