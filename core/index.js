9cd9a407ed75d12d071d1e887addcc38a066475d
kasra-sh <kasra.sh13@gmail.com>
2020-09-24 00:17:07 +0330

    add functions

--- core/index.js
+++ core/index.js
@@ -4,11 +4,13 @@ let types = require('./types');
 let cases = require('./cases');
 let strings = require('./strings');
 let queryable = require('./queryable');
+let functions = require('./functions');
 module.exports = {
     ...stream,
     ...logging,
     ...types,
     ...cases,
     ...strings,
-    ...queryable
+    ...queryable,
+    ...functions
 }
