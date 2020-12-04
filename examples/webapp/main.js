84212857246f7682a422ce009f78f5ccdd020068
Kasra Shamsaei <kasra.sh13@gmail.com>
2020-12-04 21:36:27 +0330

    v0.13.1 - revert to compatible class properties

--- examples/webapp/main.js
+++ examples/webapp/main.js
@@ -4,6 +4,7 @@ import log from "../../core/logging";
 import {div} from "../../vm/hscript-esm";
 
 log.showError()
-// log.trace(div({}, "Hellloooowww"))
-// console.log(Exir.render(App))
+
+log.trace(div({}, "Hello HyperScript"))
+
 Exir.mount(App, document.getElementById('root'))
