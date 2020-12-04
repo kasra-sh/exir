b0c4a783adfaf50631281f77ec63e61d1a1610d9
Kasra Shamsaei <kasra.sh13@gmail.com>
2020-12-04 17:01:26 +0330

    removed

--- examples/vm/App.jsx
+++
@@ -1,7 +0,0 @@
-import {mount, Component, VNode} from "../../vm/app"
-// import P from "./P";
-import Container from "./Container";
-
-window.addEventListener('load', () => {
-    mount('#root', <Container count={100}/>);
-})
--- examples/vm/Banner.jsx
+++
@@ -1,7 +0,0 @@
-import {Component, VNode} from "../../vm/app";
-
-export default class Banner extends Component {
-    view() {
-        return <h1>Banner</h1>
-    }
-}
